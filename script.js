98fdc23e9f3907dfcf0ed90b6a4979b8f7126fcb
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-10-07 21:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const num2 = prompt("Enter the second number:");
 const number1 = parseFloat(num1);
 const number2 = parseFloat(num2);
 
+// Check if the input values are valid numbers
+if (!isNaN(number1) && !isNaN(number2)) {
 // Check if the numbers are equal
 const areEqual = number1 === number2;
 
